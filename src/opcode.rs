8a1631e48687d7ba51a4ed50828979aaafe60ec1
xephir64 <chaubet.arnaud@gmail.com>
2024-11-06 15:53:57 +0100

    finished 3.2

---
+++ src/opcode.rs
@@ -0,0 +1,60 @@
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
+use crate::cpu::AddressingMode;
+
+pub struct OpCode {
+    pub op_code: u8,
+    pub mnemonic: &'static str,
+    pub byte: u16,
+    pub cycles: u8,
+    pub addr: AddressingMode,
+}
+
+impl OpCode {
+    pub fn new(
+        op_code: u8,
+        mnemonic: &'static str,
+        byte: u16,
+        cycles: u8,
+        addr: AddressingMode,
+    ) -> Self {
+        OpCode {
+            op_code,
+            mnemonic,
+            byte,
+            cycles,
+            addr,
+        }
+    }
+}
+
+lazy_static! {
+    pub static ref CPU_OPS_CODES: Vec<OpCode> = vec![
+        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing),
+        OpCode::new(0xAA, "TAX", 1, 2, AddressingMode::NoneAddressing),
+        OpCode::new(0xE8, "INX", 1, 2, AddressingMode::NoneAddressing),
+        OpCode::new(0xA9, "LDA", 2, 2, AddressingMode::Immediate),
+        OpCode::new(0xA5, "LDA", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0xB5, "LDA", 2, 4, AddressingMode::ZeroPage_X),
+        OpCode::new(0xAD, "LDA", 3, 4, AddressingMode::Absolute),
+        OpCode::new(0xBD, "LDA", 3, 4, AddressingMode::Absolute_X),
+        OpCode::new(0xB9, "LDA", 3, 4, AddressingMode::Absolute_Y),
+        OpCode::new(0xA1, "LDA", 2, 6, AddressingMode::Indirect_X),
+        OpCode::new(0xB1, "LDA", 2, 5, AddressingMode::Indirect_Y),
+        OpCode::new(0x85, "STA", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0x95, "STA", 2, 4, AddressingMode::ZeroPage_X),
+        OpCode::new(0x8D, "STA", 3, 4, AddressingMode::Absolute),
+        OpCode::new(0x9D, "STA", 3, 5, AddressingMode::Absolute_X),
+        OpCode::new(0x99, "STA", 3, 5, AddressingMode::Absolute_Y),
+        OpCode::new(0x81, "STA", 2, 6, AddressingMode::Indirect_X),
+        OpCode::new(0x91, "STA", 2, 6, AddressingMode::Indirect_Y),
+    ];
+    pub static ref OPCODES_MAP: HashMap<u8, &'static OpCode> = {
+        let mut map = HashMap::new();
+        for cpuop in &*CPU_OPS_CODES {
+            map.insert(cpuop.op_code, cpuop);
+        }
+        map
+    };
+}
